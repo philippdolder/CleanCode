6f4086037afb7c40c55d4975bae3d2242a4585aa
Philipp Dolder <philipp.dolder@gmail.com>
2013-03-10 15:31:55 +0100

    Overloads sample

---
+++ source/bbv.CleanCodeWorkshop.Test/Overloads/OrderTest.cs
@@ -0,0 +1,62 @@
+﻿namespace Bbv.CleanCodeWorkshop.Overloads
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    // TODO: Remove the overload with the signature 'AddPosition(string articleNumber, int amount, string size)' as it is never used
+    // TODO: Refactor the 'Order' class in a way that you have to change the minimum possible amount of code when you remove another overload in the future
+    [TestFixture]
+    public class OrderTest
+    {
+        private Order testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.testee = new Order();
+        }
+
+        [Test]
+        public void AddsPositionToOrder()
+        {
+            const string ArticleNumber = "A12B";
+            const int Amount = 2;
+            const string Size = "Large";
+            const string Color = "White";
+
+            this.testee.AddPosition(ArticleNumber, Amount, Size, Color);
+
+            this.testee.Positions.Should().Contain(x =>
+                x.ArticleNumber == ArticleNumber
+                && x.Amount == Amount
+                && x.Size == Size
+                && x.Color == Color);
+        }
+
+        [Test]
+        public void AddsPositionToOrderWithAmountOfOne_WhenAmountIsNotSpecified()
+        {
+            const string ArticleNumber = "A12B";
+
+            this.testee.AddPosition(ArticleNumber);
+
+            this.testee.Positions.Should().Contain(x => 
+                x.ArticleNumber == ArticleNumber
+                && x.Amount == 1);
+        }
+
+        [Test]
+        public void AddsPositionToOrderWithAmountAndNoSize_WhenSizeIsNotSpecified()
+        {
+            const string ArticleNumber = "A12B";
+            const int Amount = 3;
+
+            this.testee.AddPosition(ArticleNumber, Amount);
+
+            this.testee.Positions.Should().Contain(x =>
+                x.ArticleNumber == ArticleNumber
+                && x.Amount == Amount
+                && x.Size == string.Empty);
+        }
+    }
+}
---
+++ source/bbv.CleanCodeWorkshop/Overloads/Order.cs
@@ -0,0 +1,50 @@
+﻿namespace Bbv.CleanCodeWorkshop.Overloads
+{
+    using System.Collections.Generic;
+
+    public class Order
+    {
+        private readonly List<Position> positions = new List<Position>();
+
+        public List<Position> Positions
+        {
+            get { return this.positions; }
+        }
+
+        public void AddPosition(string articleNumber, int amount)
+        {
+            this.AddPosition(articleNumber, amount, string.Empty);
+        }
+
+        public void AddPosition(string articleNumber)
+        {
+            this.AddPosition(articleNumber, 1);
+        }
+
+        public void AddPosition(string articleNumber, int amount, string size)
+        {
+            this.AddPosition(articleNumber, amount, size, "black");
+        }
+
+        public void AddPosition(string articleNumber, int amount, string size, string color)
+        {
+            this.Positions.Add(new Position(articleNumber, amount, size, color));
+        }
+    }
+
+    public class Position
+    {
+        public Position(string articleNumber, int amount, string size, string color)
+        {
+            this.ArticleNumber = articleNumber;
+            this.Amount = amount;
+            this.Size = size;
+            this.Color = color;
+        }
+
+        public string ArticleNumber { get; private set; }
+        public int Amount { get; private set; }
+        public string Size { get; private set; }
+        public string Color { get; private set; }
+    }
+}
